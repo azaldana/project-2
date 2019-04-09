d1a69a3d70db2084d2be44cb0d98751aa230f8e4
Nai-Muh Dennis <naimuhdennis@gmail.com>
2019-04-08 21:52:41 -0700

    models updates & additions

---
+++ models/fridge.js
@@ -0,0 +1,27 @@
+/* eslint-disable linebreak-style */
+module.exports = function(sequelize, DataTypes) {
+  var Fridge = sequelize.define("Fridge", {
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
+    },
+    user: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      len: [1]
+    }
+  });
+
+  Fridge.associate = function(models) {
+    Fridge.belongsTo(models.User, {
+      foreignKey: {
+        allowNull: false
+      }
+    });
+  };
+
+  return Fridge;
+};
