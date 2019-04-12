21cbae790b62eb4325668c711345abdb00fb6746
Nai-Muh Dennis <naimuhdennis@gmail.com>
2019-04-12 16:53:12 -0700

    not needed

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -30,48 +30,6 @@ module.exports = function(app) {
     //   spoonacularId: 123, smallImg: "fgh", bigImg: "ghj", instructions: "lkdhskljf", preptime: 34, ingredients: []
     // });
   });
-
-  //   $.ajax({
-  //     type: "GET",
-  //     url: "/api/recipes/" + req.params.spoon,
-  //     success: function(response) {
-  //       var recipe = {
-  //         bigImage: 
-  //         title:
-  //         instructions:
-  //         preptime:
-  //         id:
-  //       };
-  //       var ingredients = [];
-  //       $.ajax({
-  //         type: "GET",
-  //         url: "/api/ingredients/" + recipe.id,
-  //         success: function(res) {
-  //           // res should be all th eingredients in recipe with that id
-  //           // var ingArr = names of all the ingredients
-  //           var userId = localStorage.getItem("userId");
-  //           $.ajax({
-  //             type: "GET",
-  //             url: "/api/fridge/" + userId,
-  //             success: function(r) {
-  //               // var fridgeArr = names of all the ingredients
-  //               for (var i = 0; i<ingArr.length; i++) {
-  //                 if (fridgeArr.indexOf(ingArr[i]) !== -1) {
-  //                   ingredients.push({ name: ingArr[i], fridge: true });
-  //                 } else {
-  //                   ingredients.push({ name: ingArr[i], fridge: false });
-  //                 }
-  //               }
-  //               recipe.ingredients = ingredients;
-  //               res.render("recipes", recipe);
-  //             }
-  //           });
-  //         }
-  //       });
-  //     }
-  //   });
-  // });
-
   // Render 404 page for any unmatched routes
   // app.get("*", function(req, res) {
   //   res.render("404");
