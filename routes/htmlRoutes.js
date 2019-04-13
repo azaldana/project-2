8acf4fec9c857449f436f8ec48cd8cac368d2465
Nai-Muh Dennis <naimuhdennis@gmail.com>
2019-04-12 17:46:40 -0700

    updated htmlRoutes

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -16,34 +16,6 @@ module.exports = function(app) {
       res.render("user", dbUser);
     });
   });
-  // db.Recipe.findAll({
-    //   where: {
-    //     spoonacularId: req.params.spoon
-    //   }
-    // }).then(function(dbRecipe) {
-      // res.render("recipes", dbRecipe);
-
-
-
-//   var Recipes = sequelize.define("Recipes", {
-//     spoonacularId: DataTypes.INTEGER,
-//     smallImg: DataTypes.STRING,
-//     bigImg: DataTypes.STRING,
-//     instructions: DataTypes.TEXT,
-//     prepTime: DataTypes.INTEGER
-//   });
-
-//   Recipes.associate = function(models) {
-//     Recipes.hasMany(models.Ingredients, {
-//       onDelete: "cascade"
-//     });
-//   };
-
-//   return Recipes;
-// };
-
-
-
 
   // Load recipe page and pass in a recipe by id
   app.get("/recipe/:spoon", function(req, res) {
@@ -52,53 +24,17 @@ module.exports = function(app) {
     //     spoonacularId: req.params.spoon
     //   }
     // }).then(function(dbRecipe) {
-      // res.render("recipes", dbRecipe);
-      res.render("recipes",{
-        spoonacularId: 123, smallImg: "fgh", bigImg: "ghj", instructions: "lkdhskljf", preptime: 34, ingredients: []
-      });
+    // res.render("recipes", dbRecipe);
+    res.render("recipes", {
+      spoonacularId: 123,
+      smallImg: "fgh",
+      bigImg: "ghj",
+      instructions: "lkdhskljf",
+      preptime: 34,
+      ingredients: []
+    });
     // });
   });
-
-  //   $.ajax({
-  //     type: "GET",
-  //     url: "/api/recipes/" + req.params.spoon,
-  //     success: function(response) {
-  //       var recipe = {
-  //         bigImage: 
-  //         title:
-  //         instructions:
-  //         preptime:
-  //         id:
-  //       };
-  //       var ingredients = [];
-  //       $.ajax({
-  //         type: "GET",
-  //         url: "/api/ingredients/" + recipe.id,
-  //         success: function(res) {
-  //           // res should be all th eingredients in recipe with that id
-  //           // var ingArr = names of all the ingredients
-  //           var userId = localStorage.getItem("userId");
-  //           $.ajax({
-  //             type: "GET",
-  //             url: "/api/fridge/" + userId,
-  //             success: function(r) {
-  //               // var fridgeArr = names of all the ingredients
-  //               for (var i = 0; i<ingArr.length; i++) {
-  //                 if (fridgeArr.indexOf(ingArr[i]) !== -1) {
-  //                   ingredients.push({ name: ingArr[i], fridge: true });
-  //                 } else {
-  //                   ingredients.push({ name: ingArr[i], fridge: false });
-  //                 }
-  //               }
-  //               recipe.ingredients = ingredients;
-  //               res.render("recipes", recipe);
-  //             }
-  //           });
-  //         }
-  //       });
-  //     }
-  //   });
-  // });
 
   // Load favorites page and pass in favorites by id
   app.get("/favorites/:userid", function(req, res) {
